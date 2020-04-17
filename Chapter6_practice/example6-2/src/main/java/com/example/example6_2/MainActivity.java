65dc7bf8671c6b69470c172968df74583690b26d
structsize <52183019+structsize@users.noreply.github.com>
2020-04-17 11:15:33 +0900

    [UPLOAD] App

--- Chapter6_practice/example6-2/src/main/java/com/example/example6_2/MainActivity.java
+++ Chapter6_practice/example6-2/src/main/java/com/example/example6_2/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.example6_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
 
 public class MainActivity extends AppCompatActivity {
-
+    Chronometer chronometer;
+    Button startBtn;
+    Button resetBtn;
+    Button stopBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chronometer = findViewById(R.id.chronometer);
+        startBtn = findViewById(R.id.startBtn);
+        resetBtn = findViewById(R.id.resetBtn);
+        stopBtn = findViewById(R.id.stopBtn);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chronometer.start();
+            }
+        });
+
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chronometer.setBase(SystemClock.elapsedRealtime());
+            }
+        });
+
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chronometer.stop();
+            }
+        });
     }
 }
